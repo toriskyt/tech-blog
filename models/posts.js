4877109c5afd4a24295907cb7501a68298775d2a
Tracy Torisky <mommers1@verizon.net>
2022-02-11 14:18:57 -0500

    created post model

--- models/posts.js
+++ models/posts.js
@@ -1,1 +1,37 @@
+const {Model, DataTypes} = require("sequelize")
+const sequelize = require("../config/connection")
+const bcrypt = require("bcrypt")
 
+class Posts extends Model {
+    checkPassword(plainText){
+        return bcrypt.compareSync(plainText, this.password)
+    }
+}
+
+Posts.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: "user",
+            key: "id"
+        }
+    }
+},{
+    sequelize, 
+    timestamps: false,
+    freezeTableName: true,
+    modelName: "post",
+})
+
+module.exports = Posts
