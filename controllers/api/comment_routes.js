73cb0af12f75152ad5e41dcd6ec3ea5be6290bba
Tracy Torisky <mommers1@verizon.net>
2022-02-15 16:38:52 -0500

    debugged comment route

--- controllers/api/comment_routes.js
+++ controllers/api/comment_routes.js
@@ -32,7 +32,7 @@ router.post("/", withAuth, (req, res) => {
             user_id: req.session.user_id,
         })
         .then(dbCommentData => res.json(dbCommentData))
-        .catch(err = > {
+        .catch(err => {
             console.log(err);
             res.status(400).json(err);
         })
